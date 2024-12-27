c682705dfd6e8b0df8147fc4641d079b5bb83758
Fictional-Hero <68499521+Fictional-Hero@users.noreply.github.com>
2024-12-27 15:16:02 +0200

    Adding unit tests

--- Tests/FictionalInjectionTests/SwiftAutoInitTests.swift
+++ Tests/FictionalInjectionTests/SwiftAutoInitTests.swift
@@ -1,12 +1,78 @@
 import XCTest
 @testable import FictionalInjection
 
-final class FictionalInjectionTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+protocol TestProcotol {}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class FictionalInjectionTests: XCTestCase {
+    
+    func testContainerResolvesCorrectType() {
+        var container = FDIContainer()
+        
+        class TestClass: TestProcotol {}
+        container.bind(TestProcotol.self) { _ in
+            TestClass()
+        }
+        
+        let resolvedService = container.resolve(TestProcotol.self)
+        XCTAssertNotNil(resolvedService)
+        XCTAssertTrue(resolvedService is TestClass)
     }
+    
+    func testTransientObjectIsDeallocated() {
+        class TestClass {}
+        weak var weakReference: TestClass?
+
+        var container = FDIContainer()
+        container.bind(TestClass.self) { _ in
+            TestClass()
+        }
+        
+        autoreleasepool {
+            let instance = container.resolve(TestClass.self)
+            weakReference = instance
+            XCTAssertNotNil(weakReference)
+        }
+        
+        XCTAssertNil(weakReference)
+    }
+    
+    func testGenericFactoryReleasesObject() {
+        class TestClass {}
+        weak var weakReference: TestClass?
+
+        autoreleasepool {
+            let factory = GenericFactory(TestClass.self) { _ in
+                let instance = TestClass()
+                weakReference = instance
+                return instance
+            }
+
+            let resolver = FDIContainer()
+            let strongReference = factory.resolve(resolver)
+            XCTAssertNotNil(weakReference)
+        }
+
+        XCTAssertNil(weakReference)
+    }
+    
+    func testFactoryWrapperReleasesObject() {
+        class TestClass {}
+        weak var weakReference: TestClass?
+
+        autoreleasepool {
+            let factory = GenericFactory(TestClass.self) { _ in
+                let instance = TestClass()
+                weakReference = instance
+                return instance
+            }
+            let wrapper = FactoryWrapper(factory, name: "test")
+            let resolver = FDIContainer()
+            let strongReference = wrapper.resolve(resolver) as TestClass
+            XCTAssertNotNil(weakReference)
+        }
+
+        XCTAssertNil(weakReference)
+    }
+
+
 }
